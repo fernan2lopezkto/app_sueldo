9e8c9fed6067cdce92bc7af09822fc7264b1315c
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-02-29 14:52:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 
     var sueldoNominal = sueldoBase + dineroPorHorasExtras - dineroPorFaltas;
 
-    var sueldoLiquido = sueldoNominal * (1 - (descuentos/100)) - 15300;
+    var sueldoLiquido = (sueldoNominal * (1 - (descuentos/100)));
 
 
     document.getElementById("Sueldo").value = Math.round(sueldoLiquido);
